ef07590c89edb8255a587d6699695d61e5a2945e
jtspinelli <contato@jonathanspinelli.com>
2022-11-14 00:08:09 -0300

    add grabbing style when moving recado

--- table-row.js
+++ table-row.js
@@ -102,7 +102,7 @@ export function addGrabbingCursor(event) {
     tr = event.target.parentElement;
 
     const trDragging = document.getElementById('dragging');
-    trDragging.innerHTML = `<td class="td-grab"><svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" class="style-scope yt-icon" style="pointer-events: none; display: block; width: 100%; height: 100%;"><g class="style-scope yt-icon"><path d="M21,10H3V9h18V10z M21,14H3v1h18V14z" class="style-scope yt-icon"></path></g></svg></td><th scope="row">${'1'}</th><td class="descricao">${'DESCRIÇÃO'}</td><td class="detalhamento">${'DETALHAMENTO'}</td><td><button class="btn btn-primary">editar</button><button class="btn btn-secondary">excluir</button></td>`;
+    trDragging.innerHTML = `<td class="td-grab td-grabbed"><svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" class="style-scope yt-icon" style="pointer-events: none; display: block; width: 100%; height: 100%;"><g class="style-scope yt-icon"><path d="M21,10H3V9h18V10z M21,14H3v1h18V14z" class="style-scope yt-icon"></path></g></svg></td><th scope="row">${'1'}</th><td class="descricao">${'DESCRIÇÃO'}</td><td class="detalhamento">${'DETALHAMENTO'}</td><td><button class="btn btn-primary">editar</button><button class="btn btn-secondary">excluir</button></td>`;
 
     trDragging.children[1].textContent = tr.children[1].textContent;
     trDragging.children[2].textContent = tr.children[2].textContent;
