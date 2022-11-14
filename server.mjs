43463c421ab0864df9fbf24f55a0ccd7c8020f15
jtspinelli <contato@jonathanspinelli.com>
2022-11-14 17:56:02 -0300

    update express port

--- server.mjs
+++ server.mjs
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
